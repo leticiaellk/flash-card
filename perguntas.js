195026046c0f104a33425897bca478b0023984a1
Letícia Helker <168765119+leticiaellk@users.noreply.github.com>
2024-11-21 14:09:30 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é sistema nervoso?',
+    'O sistema nervoso é o sistema responsável por captar, processar e gerar respostas diante dos estímulos aos quais somos submetidos.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que causa as estações do ano?',
+    'O movimento de translação, em que a Terra gira ao redor do Sol, é responsável pela existência das estações do ano.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma função?',
+    'Uma função é uma regra que relaciona cada elemento de um conjunto a um único elemento de outro. '
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é um verbo?',
+    'O verbo é a palavra que expressa ações, estados e fenômenos da natureza. É, portanto, uma das classes gramaticais mais importantes da língua portuguesa. Exemplos: "Maria correu pelo parque durante a manhã." (ação)'
+)
